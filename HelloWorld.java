6ed154ba36378cc6ab8afe09b452299557840b61
anumsh <anumsharma09@gmail.com>
2017-04-27 22:37:32 -0700

    feat: Hello World program

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+/* package of basics */
+package com.anum.practice.basics;
+
+/* class HelloWorld */
+public class HelloWorld {
+
+	/* Method that contains:
+	 * public- can access by anyone
+	 * static- we have not create object to execute the class.
+	 * void- no return type
+	 * main- method name
+	 * string[] args- if you want pass arguments to this class.
+	 * this is main method where JVM starts executing the program. 
+	 * */
+	public static void main(String[] args) {
+		/* 
+		 * System- it is system command.
+		 * out- that represent text as output
+		 * println- printing it on screen as a new line */
+		// printing string type data
+		System.out.println("Udacity Rocks!");
+		// printing integer type data
+		System.out.println(2+3);
+		
+
+	}
+
+}
+

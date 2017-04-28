bf2bf001e826de4e330e1972af71ed2705e711eb
anumsh <anumsharma09@gmail.com>
2017-04-28 14:56:41 -0700

    feat: add comments in coffeeMaker program

--- CoffeeMaker.java
+++ CoffeeMaker.java
@@ -3,9 +3,28 @@ public class CoffeeMaker {
 	public static void main(String[] args) {
 		/*
 		 * This is example of switch statement.
-		 * CoffeeMaker will make the coffee your the people of specific type 
+		 * 
+		 * CoffeeMaker will make the coffee for  the people of specific type 
 		 * according to passcode provided by people.
-		 * break statement helps us to go out of loop without executing next statements.
+		 * 
+		 * 
+		 *After this code is this word break; which will break out of the case code 
+		 *and go to the ending curly brace of the switch statement, 
+		 *where this code prints the value of coffeeType.
+		 *So the break ensures that only one case at a time will have it’s code executed.
+		 *If the break were not included, Java would continue reading code in the
+		 * switch statement line by line and keep executing statements 
+		 * (like the default case code) until it reaches the end of the
+		 *  switch statement or hits another break!
+		 *  
+		 *  
+		 *  The default case typically comes at the end of a switch statement and 
+		 *  its code will execute whenever the other cases aren’t met.
+		 *  So, if the passcode was something like 914, 
+		 *  this code doesn’t have a case for this particular value,
+		 *  and so it would fall into the default category and 
+		 *  the coffeeType String would be set to “Unknown".
+
 		 */
 		int passcode=444;
 		String coffeeType;
@@ -29,3 +48,4 @@ public class CoffeeMaker {
 	}
 
 }
+

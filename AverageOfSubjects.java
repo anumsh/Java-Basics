02047aec28dc019c92175a5a0ad1124f8078e21d
anumsh <anumsharma09@gmail.com>
2017-04-28 00:30:20 -0700

    feat: Add Average of 5 subject program

---
+++ AverageOfSubjects.java
@@ -0,0 +1,22 @@
+public class AverageOfSubjects {
+
+	public static void main(String[] args) {
+		/* Your friend seems to be really good at music, 
+		he managed to score 96% on his final exam!
+		Now that you know all 5 subject grades, 
+		update the average calculation below to calculate the 
+		total average of all subjects. */
+
+		double maths=97.5;
+		double english=98;
+		double science=83.5;
+		double drama=75;
+		double music=96;
+		double sum=maths+english+science+drama + music;
+		double average=sum/5;
+		System.out.println(average);
+
+	}
+
+}
+

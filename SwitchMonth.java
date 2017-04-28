afcd681b19d9762f296c0c61dcd01ebb4054e781
anumsh <anumsharma09@gmail.com>
2017-04-28 15:00:53 -0700

    feat: Another switch statement example

---
+++ SwitchMonth.java
@@ -0,0 +1,40 @@
+public class SwitchMonth {
+	/*
+	 * Another switch  statement example
+	 */
+
+	public static void main(String[] args) {
+		int month=12; // should be a number 1-12
+		String monthString;
+
+		switch(month) {
+		    case 1: monthString = "January";
+		        break;
+		    case 2: monthString = "February";
+		        break;
+		    case 3: monthString = "March";
+		        break;
+		    case 4: monthString = "April";
+		        break;
+		    case 5: monthString = "May";
+		        break;
+		    case 6: monthString = "June";
+		        break;
+		    case 7: monthString = "July";
+		        break;
+		    case 8: monthString = "August";
+		        break;
+		    case 9: monthString = "September";
+		        break;
+		    case 10: monthString = "October";
+		        break;
+		    case 11: monthString = "November";
+		        break;
+		    case 12: monthString = "December";
+		        break;
+		    default: monthString = "invalid month";
+		        
+		}
+		System.out.println(monthString);
+}
+}

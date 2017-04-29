fb2edf57287d0f4d21d95665c7838542234f1de5
anumsh <anumsharma09@gmail.com>
2017-04-29 01:28:01 -0700

    feat: Adding random roll as parameters

--- RollDice.java
+++ RollDice.java
@@ -1,11 +1,11 @@
 public class RollDice {
 	/*  random number example */
 	
-	public static int roll() {
+	public static int roll(int sides) {   // random roll parameter
 		//random number from 0 to almost 1
 	double randomNumber=Math.random();
 	//change range from 0 to almost 6
-	randomNumber=randomNumber * 6;
+	randomNumber=randomNumber * sides;
 	//shift range upto one
 	randomNumber=randomNumber + 1;
 	//cast to integer(ignore decimal part)
@@ -14,8 +14,9 @@ public class RollDice {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(roll());
+		System.out.println(roll(8));
 
 	}
 
 }
+

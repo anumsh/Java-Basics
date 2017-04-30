e25929d4fd67150539b46bafceac9147155a16f0
anumsh <anumsharma09@gmail.com>
2017-04-29 21:58:18 -0700

    feat: FOR loop example

---
+++ raiseAlarm.java
@@ -0,0 +1,18 @@
+public class raiseAlarm {
+
+	public static void main(String[] args) {
+		/*
+		 * FOR LOOP example 
+		 * it consist of 3 things:
+		 * 1. loop initialization
+		 * 2. loop condition
+		 * 3. loop increment
+		 */ 
+		for(int i=1; i<=10 ; i++){
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
